5a50d63bb0be5e89c6fda03dffd99ae40c697212
root <vladimir.beauge@gmail.com>
2016-11-22 08:16:31 -0500

    changes

--- attacker/ubun-64-digO-2/ping-victim.sh
+++ attacker/ubun-64-digO-2/ping-victim.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 #ping below threshold 35536
-ping -c 35536 192.168.0.11  >> ping-victim.xlsx
-ping -c 35536 192.168.0.4   >> ping-victim.xlsx
+ping -c 35536 192.168.0.4 
 
 #ping at threshold 65536
-ping -c 65536 192.168.0.11 >> ping-victim.xlsx
-ping -c 65536 192.168.0.4  >> ping-victim.xlsx
+ping -c 65536 192.168.0.4 
 
 #ping before threshold 95536
-ping -c 95536 192.168.0.11 >> ping-victim.xlsx
-ping -c 95536 192.168.0.4  >> ping-victim.xlsx
-
-#add to git
-git add .
-git commit -m "adding ping stats to excel sheets"
-git push origin master
+ping -c 95536 192.168.0.4
