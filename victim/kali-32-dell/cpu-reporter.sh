231db486603979f22bf6c57b8f8485a4f6135c18
root <vladimir.beauge@gmail.com>
2016-11-22 08:19:50 -0500

    changes etc

--- victim/kali-32-dell/cpu-reporter.sh
+++ victim/kali-32-dell/cpu-reporter.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #show cpu usage before pings
-mpstat >> cpu-stats.xlsx
+echo mpstat >> cpu-stats.xlsx
 
 #show cpu usage while getting pinged
-mpstat >> cpu-stats.xlsx
+echo mpstat >> cpu-stats.xlsx
 
 #show cpu usage after getting pinged
 #35536
-mpstat 10 35536 >> cpu-stats.xlsxi
+echo mpstat 10 35536 >> cpu-stats.xlsxi
 
 #65536
-mpstat 10 65536 >> cpu-stats.xlsx
+echo mpstat 10 65536 >> cpu-stats.xlsx
 
 #95536
-mpstat 10 95536 >> cp
+echo mpstat 10 95536 >> cp
 
 #add space line
 echo "\n" >> cpu-stats.xlsx
