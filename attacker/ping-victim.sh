9bc32970dddca7c51e4e7954289a4c7d4a229ac1
Vlad <vladBeauge@gmail.com>
2016-11-28 20:23:20 -0500

    you know why i'm here

--- attacker/ping-victim.sh
+++ attacker/ping-victim.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-#ping below threshold 35536
-ping -c 35536 192.168.0.4 >> ping-receipt-3556.xlsx
-
-#ping at threshold 65536
-ping -c 65536 192.168.0.4 >> ping-receipt-65536.xlsx
-	
-#ping before threshold 95536
-ping -c 95536 192.168.0.4 >> ping-receipt-95536.xlsx
+ping -c 100000 192.168.0.4 >> /home/git-repo/Ping-Reporter/attacker/kali-32-dell/ping-receipt-same-line.xlsx
