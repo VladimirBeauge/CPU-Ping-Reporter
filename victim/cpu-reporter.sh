22158a7593f74bce2cad375f64f252a1f39d926a
Vlad <vladBeauge@gmail.com>
2016-11-28 19:39:11 -0500

    ..

--- victim/cpu-reporter.sh
+++ victim/cpu-reporter.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 mpstat 5 100000 >> cpu-reporter-<line>-<location>.xlsx
-=======
-mpstat 5 100000 >> cpu-reporter-same-line-home.xlsx
->>>>>>> ae8f831cbee55ea9b7b4c2119307afcfa92373ca
