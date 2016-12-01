b36c20936859d9d61385529e65767099c62486df
root <vladimir.beauge@gmail.com>
2016-11-30 21:46:01 -0500

    update

--- Exp3/step2-hping3.sh
+++ Exp3/step2-hping3.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-hping3 x.x.x.x --rand-dest --interface -a --flood
-=======
-hping3 192.168.0.4 --rand-dest --interface data-hping3-home.xlsx --flood
->>>>>>> 3629a1e07f2feb6d6ecd8f7f18ea9795c9b2fac2
-
+hping3 -1 --flood -V --rand-source 192.0.43.10
