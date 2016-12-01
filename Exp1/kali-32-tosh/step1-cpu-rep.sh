38f0ea4144c8af947fe815be620296cddc5595c9
root <vladimir.beauge@gmail.com>
2016-12-01 15:10:24 -0500

    update

--- Exp1/kali-32-tosh/step1-cpu-rep.sh
+++ Exp1/kali-32-tosh/step1-cpu-rep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #show cpu usage after getting pinged
-mpstat 60 100000 >> data-set2-cpu-exp1-school.xlsx
+mpstat 60 100000 >> ~/Desktop/git-repos/Ping-Reporter/Exp1/kali-32-tosh/data/data-set1-cpu-exp1.xlsx
--- Exp1/step2-hping3.sh
+++ Exp1/step2-hping3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hping3 -1 --flood -V --rand-source 192.0.43.10
+hping3 -1 --flood -V --rand-source 137.143.58.140
