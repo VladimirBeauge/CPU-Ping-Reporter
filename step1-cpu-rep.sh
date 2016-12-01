3dd5ab7ddd9fd758c04ddb1840ca7427b2f622eb
Vlad <vladBeauge@gmail.com>
2016-12-01 15:29:32 -0500

    trimming

--- step1-cpu-rep.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#show cpu usage after getting pinged
-mpstat 60 100000 >> /home/git-repo/Ping-Reporter/Exp2/<attack xor victim>data-<set>-<exp>.xlsx
--- step2-hping3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-hping3 -1 --flood -V --rand-source -f <machine B ip>
