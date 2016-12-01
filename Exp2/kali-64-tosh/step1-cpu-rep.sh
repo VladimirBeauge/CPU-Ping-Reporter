3c38060ddd0cf020ba59e1ffde8a72b81f1db7f4
root <vladimir.beauge@gmail.com>
2016-11-30 19:56:54 -0500

    update

--- Exp2/kali-64-tosh/step1-cpu-rep.sh
+++ Exp2/kali-64-tosh/step1-cpu-rep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #show cpu usage after getting pinged
-mpstat 5 100000 >> data-cpu-exp2.xlsx
+mpstat 5 100000 >> data-cpu-exp-<location>.xlsx
 
--- Exp4/step2-hping3.sh
+++ Exp4/step2-hping3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hping3 x.x.x.x --rand-dest --interface data-hping3-home.xlsx --flood
+hping3 x.x.x.x --rand-dest --interface -a --flood
 
