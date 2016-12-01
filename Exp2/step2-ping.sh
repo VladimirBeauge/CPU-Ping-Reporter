a5fde0560704bac8b1e34711f39fbf579f6bc035
Vladimir Beauge <VladimirBeauge@users.noreply.github.com>
2016-11-30 19:27:31 -0500

    Update step2-ping.sh

--- Exp2/step2-ping.sh
+++ Exp2/step2-ping.sh
@@ -3,9 +3,9 @@
 x=0
 
 while [ $x -lt 1 ]; do
-	ping -s 21833 -c 1 192.168.0.4 >> data-21833-ping-same-home.xlsx
+	ping -s 21833 -c 1 192.168.0.4 mtu-discovery want >> data-21833-ping-same-home.xlsx
 
-	ping -s 43666 -c 1 192.168.0.4 >> data-43666-ping-same-home.xlsx
+	ping -s 43666 -c 1 192.168.0.4 mtu-discovery want >> data-43666-ping-same-home.xlsx
 
-	ping -s 65500 -c 1 192.168.0.4 >> data-65500-ping-same-home.xlsx
+	ping -s 65500 -c 1 192.168.0.4 mtu-discovery want >> data-65500-ping-same-home.xlsx
 done
