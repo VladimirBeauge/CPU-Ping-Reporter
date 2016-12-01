94c6b9dddd8518585c0153471211d6c74d149db3
Vlad <vladBeauge@gmail.com>
2016-12-01 17:42:21 -0500

    update

--- Exp3/step1-cpu-rep.sh
+++ Exp3/step1-cpu-rep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #show cpu usage after getting pinged
-mpstat 60 100000 >> /home/git-repo/Ping-Reporter/Exp3/<attack xor victim>data-<set>-<exp>.xlsx
+mpstat 30 100000 >> /home/git-repo/Ping-Reporter/Exp2/data-<attack xor victim>/data-<set>-<exp>.xlsx
