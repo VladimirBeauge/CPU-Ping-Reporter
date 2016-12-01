956f0eb3b8c7d769f6a23bc3c38214b5367633ba
Vladimir Beauge <VladimirBeauge@users.noreply.github.com>
2016-11-30 22:39:00 -0500

    Update step2-ping.sh

--- Exp2/kali-64-tosh/step2-ping.sh
+++ Exp2/kali-64-tosh/step2-ping.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-x=0
-
-while [ $x -lt 1 ]; do
-	ping -s 21833 -c 1 192.168.0.4 >> data-21833-ping-same-home.xlsx
-
-	ping -s 43666 -c 1 192.168.0.4 >> data-43666-ping-same-home.xlsx
-
-	ping -s 65500 -c 1 192.168.0.4 >> data-65500-ping-same-home.xlsx
-done
+ping -s <size> -c 100000 192.168.0.4 >> data-<set#>-<size>-ping-<location>.xlsx
