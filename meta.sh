9d08214d9ad6cfbf81dbaafe25b3f5e22454bc82
Vladimir Beauge <VladimirBeauge@users.noreply.github.com>
2016-12-01 21:29:25 -0500

    Update and rename real-folder to meta.sh

---
+++ meta.sh
@@ -0,0 +1,1 @@
+wget http://downloads.metasploit.com/data/releases/metasploit-latest-x64-linux-installer.run && chmod +x ./metasploit-latest-linux-x64-installer.run && sudo ./metasploit-latest-linux-x64-installer.run
